6e8422f262cb28692bca85fb1cf8b6fb26f1809c
enoclu <enockmkwananzi@gmail.com>
2022-12-11 19:12:48 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unidtd.h>
 /**
- * MAIN
+ * main - this is the quote function that executes
  * Return: 0
  */
+
 int main (void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
