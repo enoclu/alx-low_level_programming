d38d4c69053bc1b461a73ff2bb0692c4003b4ca6
enoclu <enockmkwananzi@gmail.com>
2022-12-13 02:41:33 -0800

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - the function is to loop the alphabet character
+ *
+ * print_alphabet - this is where the coe for this ans found.
+ *
+ *print_alphabet - for all codes
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+
+	_putchar("\n");
+
+	return (0);
+}
