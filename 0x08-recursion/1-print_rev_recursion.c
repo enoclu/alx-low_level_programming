24126b0df64b7075df5d6588f5f7e87715df71ed
enoclu <enockmkwananzi@gmail.com>
2023-01-04 12:40:07 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+1-PRINT_REV_RECURSION.C CODE
+
+
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
